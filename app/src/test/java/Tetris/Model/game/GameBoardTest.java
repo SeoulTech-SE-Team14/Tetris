b93370d5887574bd5737af6b5d588c9f0c571fc1
김영균 <kyoungyun98@naver.com>
2022-04-18 00:24:54 +0900

    test : 일반 블럭 아래 이동 테스트 추가
    - 일반 블럭이 내려가는 경우 테스트

--- app/src/test/java/Tetris/Model/game/GameBoardTest.java
+++ app/src/test/java/Tetris/Model/game/GameBoardTest.java
@@ -1,32 +1,65 @@
 package Tetris.Model.game;
 
-import Tetris.Model.block.Block;
-import Tetris.Model.block.TBlock;
-import org.junit.Before;
+import Tetris.Model.block.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameBoardTest {
-    /**
-     * 000000
-     * 000000
-     * 000000
-     * 000000
-     * 000000
-     * 000000
-     */
-    GameState gameState = new GameState("normal");
 
-    GameBoard board = new GameBoard(gameState, 20, 10);
-    @Before
+    GameState gameState;
+    GameBoard board;
+    Block secondBlock;
+
+    @BeforeEach
     void init() {
-        Block block = new TBlock();
-        block.setX(3);
-        block.setY(3);
+        gameState = new GameState("normal");
+        board = new GameBoard(gameState, 20, 10);
+    }
 
+    @Test
+    @DisplayName("일반 블럭이 내려갈 수 있는 경우")
+    void moveDownTest1() {
+        // given
+        Block tempBlock = new IBlock();
+        secondBlock = new TBlock();
+
+        secondBlock.setX(3);
+        secondBlock.setY(3);
+
+        board.spawn(secondBlock);
+        board.spawn(tempBlock);
+
+        //when
+        board.moveDown();
+
+        //then
+        assertEquals(4, secondBlock.getY());
     }
     @Test
-    void moveDown() {
+    @DisplayName("일반 블럭이 내려갈 수 없는 경우")
+    void moveDownTest2() {
+        // given
+        Block tempBlock = new IBlock();
+        secondBlock = new TBlock();
+
+        secondBlock.setX(3);
+        secondBlock.setY(18);
+
+        board.spawn(secondBlock);
+        board.spawn(tempBlock);
+
+        //when
+        board.moveDown();
+
+        //then
+        assertEquals(18, secondBlock.getY());
+    }
+    @Test
+    @DisplayName("무게추 아이템이 움직일 때")
+    void moveDownTest3() {
 
     }
     @Test
