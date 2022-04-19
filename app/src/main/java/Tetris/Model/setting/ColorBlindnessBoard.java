7b1dbf5c40909566590874c5b266fdb666fba526
Donghwan Yoo <ydh4919@naver.com>
2022-04-19 10:34:52 +0900

    색맹모드 모델,뷰 수정 colorblindness 이미지파일 수정

--- app/src/main/java/Tetris/Model/setting/ColorBlindnessBoard.java
+++ app/src/main/java/Tetris/Model/setting/ColorBlindnessBoard.java
@@ -7,7 +7,7 @@ import java.util.Observable;
 
 public class ColorBlindnessBoard extends Observable {
     private static int indicator = 0;
-    private final int buttonCount = 4;
+    private final int buttonCount = 5;
     private final int buttonWidth = 260;
     private final int buttonHeight = 60;
     private final int screenWidth = JsonReader.getWidth();
@@ -18,17 +18,24 @@ public class ColorBlindnessBoard extends Observable {
     private final ImageIcon backgroundImage300600 = new ImageIcon("app/src/main/resources/image/default_background_300_600.png");
 
     // default image
-    private final ImageIcon size300600BtnImage = new ImageIcon("app/src/main/resources/image/size_setting/button_300_600.png");
-    private final ImageIcon size350700BtnImage = new ImageIcon("app/src/main/resources/image/size_setting/button_350_700.png");
-    private final ImageIcon size400800BtnImage = new ImageIcon("app/src/main/resources/image/size_setting/button_400_800.png");
+    private final ImageIcon colorDefaultBtnImage = new ImageIcon("app/src/main/resources/image/colorblindness/button_colorblindness_default.png");
+    private final ImageIcon colorRedBtnImage = new ImageIcon("app/src/main/resources/image/colorblindness/button_colorblindness_red.png");
+    private final ImageIcon colorGreenBtnImage = new ImageIcon("app/src/main/resources/image/colorblindness/button_colorblindness_green.png");
+    private final ImageIcon colorBlueBtnImage = new ImageIcon("app/src/main/resources/image/colorblindness/button_colorblindness_blue.png");
     private final ImageIcon defaultBackBtnImage = new ImageIcon("app/src/main/resources/image/button_back.png");
-
+    
     // focus image
-    private final ImageIcon focusSize300600BtnImage = new ImageIcon("app/src/main/resources/image/size_setting/button_300_600_focused.png");
-    private final ImageIcon focusSize350700BtnImage = new ImageIcon("app/src/main/resources/image/size_setting/button_350_700_focused.png");
-    private final ImageIcon focusSize400800BtnImage = new ImageIcon("app/src/main/resources/image/size_setting/button_400_800_focused.png");
+    private final ImageIcon focusColorDefaultBtnImage = new ImageIcon("app/src/main/resources/image/colorblindness/button_colorblindness_default_focused.png");
+    private final ImageIcon focusColorRedBtnImage = new ImageIcon("app/src/main/resources/image/colorblindness/button_colorblindness_red_focused.png");
+    private final ImageIcon focusColorGreenBtnImage = new ImageIcon("app/src/main/resources/image/colorblindness/button_colorblindness_green_focused.png");
+    private final ImageIcon focusColorBlueBtnImage = new ImageIcon("app/src/main/resources/image/colorblindness/button_colorblindness_blue_focused.png");
     private final ImageIcon focusDefaultBackBtnImage = new ImageIcon("app/src/main/resources/image/button_back_focused.png");
 
+    // preview image
+    private final ImageIcon previewDefaultImage = new ImageIcon("app/src/main/resources/image/colorblindness/preview_colorblindness_default.png");
+    private final ImageIcon previewRedImage = new ImageIcon("app/src/main/resources/image/colorblindness/preview_colorblindness_red.png");
+    private final ImageIcon previewGreenImage = new ImageIcon("app/src/main/resources/image/colorblindness/preview_colorblindness_green.png");
+    private final ImageIcon previewBlueImage = new ImageIcon("app/src/main/resources/image/colorblindness/preview_colorblindness_blue.png");
 
     public void setIndicator(int indicator) {
         ColorBlindnessBoard.indicator = indicator;
@@ -64,30 +71,49 @@ public class ColorBlindnessBoard extends Observable {
         }
     }
 
-    public ImageIcon getSize300600BtnImage() {
-        return size300600BtnImage;
+    public ImageIcon getColorDefaultBtnImage() {
+        return colorDefaultBtnImage;
     }
-    public ImageIcon getSize350700BtnImage() {
-        return size350700BtnImage;
+    public ImageIcon getColorRedBtnImage() {
+        return colorRedBtnImage;
     }
-    public ImageIcon getSize400800BtnImage() {
-        return size400800BtnImage;
+    public ImageIcon getColorGreenBtnImage() {
+        return colorGreenBtnImage;
+    }
+    public ImageIcon getColorBlueBtnImage() {
+        return colorBlueBtnImage;
     }
     public ImageIcon getDefaultBackBtnImage() {
         return defaultBackBtnImage;
     }
 
-    public ImageIcon getFocusSize300600BtnImage() {
-        return focusSize300600BtnImage;
+    public ImageIcon getFocusColorDefaultBtnImage() {
+        return focusColorDefaultBtnImage;
     }
-    public ImageIcon getFocusSize350700BtnImage() {
-        return focusSize350700BtnImage;
+    public ImageIcon getFocusColorRedBtnImage() {
+        return focusColorRedBtnImage;
     }
-    public ImageIcon getFocusSize400800BtnImage() {
-        return focusSize400800BtnImage;
+    public ImageIcon getFocusColorGreenBtnImage() {
+        return focusColorGreenBtnImage;
+    }
+    public ImageIcon getFocusColorBlueBtnImage() {
+        return focusColorBlueBtnImage;
     }
     public ImageIcon getFocusDefaultBackBtnImage() {
         return focusDefaultBackBtnImage;
+    }
+
+    public ImageIcon getPreviewDefaultImage() {
+        return previewDefaultImage;
+    }
+    public ImageIcon getPreviewRedImage() {
+        return previewRedImage;
+    }
+    public ImageIcon getPreviewGreenImage() {
+        return previewGreenImage;
+    }
+    public ImageIcon getPreviewBlueImage() {
+        return previewBlueImage;
     }
 
     public int getButtonCount() {
--- app/src/main/java/Tetris/View/setting/ColorBlindnessView.java
+++ app/src/main/java/Tetris/View/setting/ColorBlindnessView.java
@@ -1,5 +1,126 @@
 package Tetris.View.setting;
 
-public class ColorBlindnessView {
-    
+import Tetris.Model.setting.ColorBlindnessBoard;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Observable;
+import java.util.Observer;
+
+public class ColorBlindnessView extends JFrame implements Observer{
+    private final ColorBlindnessBoard model = new ColorBlindnessBoard();
+    private final JButton colorDefaultBtn = new JButton(model.getFocusColorDefaultBtnImage());
+    private final JButton colorRedBtn = new JButton(model.getFocusColorRedBtnImage());
+    private final JButton colorGreenBtn = new JButton(model.getFocusColorGreenBtnImage());
+    private final JButton colorBlueBtn = new JButton(model.getFocusColorBlueBtnImage());
+    private final JButton backBtn = new JButton(model.getFocusDefaultBackBtnImage());
+    //블록색상 미리보기 이미지 추가해야함
+
+    public ColorBlindnessView(int x, int y){
+        super("SeoulTech SE Tetris");
+
+        setSize(new Dimension(model.getScreenWidth(), model.getScreenHeight()));
+        setPreferredSize(new Dimension(model.getScreenWidth(), model.getScreenHeight()));
+        setLocation(x, y);
+        setLayout(null);
+        JPanel background = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                setOpaque(false);
+                g.drawImage(model.getBackgroundImage().getImage(), 0, 0, null);
+                super.paintComponent(g);
+            }
+        };
+        setContentPane(background);
+
+        JPanel buttons = new JPanel();
+        buttons.setLayout(new GridLayout(4, 1));
+
+        colorDefaultBtn.setBorderPainted(false);
+        colorDefaultBtn.setContentAreaFilled(false);
+        colorDefaultBtn.setFocusPainted(false);
+        colorDefaultBtn.setOpaque(false);
+        buttons.add(colorDefaultBtn);
+
+        colorRedBtn.setBorderPainted(false);
+        colorRedBtn.setContentAreaFilled(false);
+        colorRedBtn.setFocusPainted(false);
+        colorRedBtn.setOpaque(false);
+        buttons.add(colorRedBtn);
+
+        colorGreenBtn.setBorderPainted(false);
+        colorGreenBtn.setContentAreaFilled(false);
+        colorGreenBtn.setFocusPainted(false);
+        colorGreenBtn.setOpaque(false);
+        buttons.add(colorGreenBtn);
+
+        colorBlueBtn.setBorderPainted(false);
+        colorBlueBtn.setContentAreaFilled(false);
+        colorBlueBtn.setFocusPainted(false);
+        colorBlueBtn.setOpaque(false);
+        buttons.add(colorBlueBtn);
+
+        backBtn.setBorderPainted(false);
+        backBtn.setContentAreaFilled(false);
+        backBtn.setFocusPainted(false);
+        buttons.add(backBtn);
+
+        //블록색상 미리보기 칸 추가해야함
+
+        buttons.setOpaque(false);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        GridBagLayout gbl = new GridBagLayout();
+        background.setLayout(gbl);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridheight = 1;
+        gbc.fill = GridBagConstraints.BOTH;
+        add(buttons, gbc);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setFocusable(true);
+        requestFocus();
+        pack();
+        setVisible(true);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        int indicator = model.getIndicator();
+        if(indicator == 0){
+            colorDefaultBtn.setIcon(model.getFocusColorDefaultBtnImage());
+        } else {
+            colorDefaultBtn.setIcon(model.getColorDefaultBtnImage());
+        }
+        if(indicator == 1){
+            colorRedBtn.setIcon(model.getFocusColorRedBtnImage());
+        } else {
+            colorRedBtn.setIcon(model.getColorRedBtnImage());
+        }
+        if(indicator == 2){
+            colorGreenBtn.setIcon(model.getFocusColorGreenBtnImage());
+        } else {
+            colorGreenBtn.setIcon(model.getColorGreenBtnImage());
+        }
+        if(indicator == 3){
+            colorBlueBtn.setIcon(model.getFocusColorBlueBtnImage());
+        } else {
+            colorBlueBtn.setIcon(model.getColorBlueBtnImage());
+        }
+        if(indicator == 4){
+            backBtn.setIcon(model.getFocusDefaultBackBtnImage());
+        } else {
+            backBtn.setIcon(model.getDefaultBackBtnImage());
+        }
+        pack();
+        super.paint(g);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        repaint();
+    }
 }
+
